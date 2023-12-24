d85ee95b079fde5aa15076adc49a3337fd5af70e
ahcgnoej <jinjinchachajung@naver.com>
2023-12-24 16:18:18 +0900

    첫 잔디

---
+++ class/03-javascript/01-variable.js
@@ -0,0 +1,3 @@
+let time=10
+setInterval(function(){console.log(time)
+    time=time-1},1000)
---
+++ cyworld/index.js
@@ -0,0 +1,9 @@
+const menuHome=()=>{
+    document.getElementById("contentFrame").setAttribute("src","../home.html")
+}
+const menuJukebox=()=>{
+    document.getElementById("contentFrame").setAttribute("src","../jukebox.html")
+}
+const menuGame=()=>{
+    document.getElementById("contentFrame").setAttribute("src","../game.html")
+}
